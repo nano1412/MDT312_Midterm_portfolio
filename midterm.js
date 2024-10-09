dd0b09469564fe8456b36e565a236a88a81efc7a
nano1412 <54862667+nano1412@users.noreply.github.com>
2024-10-09 18:19:56 +0700

    add description and update old assignment css

--- midterm.js
+++ midterm.js
@@ -29,3 +29,24 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
+}
+window.addEventListener('scroll', scrollActive)
